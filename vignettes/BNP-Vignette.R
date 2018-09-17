3a12cfca3c091180207bbfad11ae7029bfc70e86
mar-esther23 <mar.esther23@gmail.com>
2018-09-17 11:42:48 -0500

    vignette

--- vignettes/BNP-Vignette.R
+++ vignettes/BNP-Vignette.R
@@ -27,3 +27,57 @@ labelsTh17Treg
 labels <- labelAttractors(attr, labelsTh17Treg)
 table(labels)
 
+## ------------------------------------------------------------------------
+mutants <- perturbNetworkFixedNodes(netTh17Treg, label.rules = labelsTh17Treg)
+
+mutants <- mutants[order(-mutants$WT),]  # order by WT in descending order
+mutants[mutants==0] <- NA  # replace 0 for NA for plotting
+colfunc <- colorRampPalette(c("cyan", "darkblue"))  # color scale
+heatmap(data.matrix(mutants),
+        Rowv=NA, Colv=NA, 
+        col= colfunc(10),scale="none")
+
+## ------------------------------------------------------------------------
+data("envTh17Treg")
+envTh17Treg
+
+## ------------------------------------------------------------------------
+env.attr <- perturbNetworkFixedNodes(netTh17Treg, label.rules = labelsTh17Treg,
+                                     genes  = envTh17Treg$nodes, 
+                                     values = envTh17Treg$value, 
+                                     names  = envTh17Treg$label)
+
+env.attr <- env.attr[order(-env.attr$All),]  # order by WT in descending order
+env.attr[env.attr==0] <- NA  # replace 0 for NA for plotting
+colfunc <- colorRampPalette(c("cyan", "darkblue"))  # color scale
+heatmap(data.matrix(env.attr),
+        Rowv=NA, Colv=NA, 
+        scale="none", col= colfunc(10))
+
+## ------------------------------------------------------------------------
+# Get WT attractors
+attr.df <- getAttractors(netTh17Treg)
+attr.df <- attractorToDataframe(attr.df)
+
+# Perturb net and get attractors
+perturbed.net <- perturbNetwork(netTh17Treg, perturb="functions")
+pertubed.attr <- getAttractors(perturbed.net)
+pertubed.attr.df <- attractorToDataframe(pertubed.attr)
+
+# Calculate number of attractors not in the original network
+diff <- setdiff(attr.df$involvedStates, pertubed.attr.df$involvedStates) 
+length(diff)
+
+## ------------------------------------------------------------------------
+attr <- getAttractors(netTh17Treg)
+verifySyncronousVsAsyncronous(netTh17Treg, attr, label.rules = labelsTh17Treg)
+
+## ------------------------------------------------------------------------
+perturbState(netTh17Treg, state = 0, 
+             genes=c("IL21e","TGFBe"), 
+             result = "trajectory")
+
+## ------------------------------------------------------------------------
+cellfate <- cellFateMap(netTh17Treg, label.rules = labelsTh17Treg)
+head(cellfate)
+
